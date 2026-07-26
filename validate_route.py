edf20d97902bb8b319fe8a3458d0f1a260a03255
Raghav Sharma <raghavusharmaa@gmail.com>
2026-07-27 02:24:14 +0530

    add: Validation and routing of methods: POST, GET,GET BY ID,DELETE

---
+++ validate_route.py
@@ -0,0 +1,50 @@
+import json
+from flask import Flask, jsonify,request
+app = Flask(__name__)
+items =[]
+@app.route('/items', methods=['POST'])
+def add_item():
+    data = request.json
+    if not data or 'name' not in data  :
+        return jsonify({'error':"name is required"}), 400
+    add={}
+    id = len(items) + 1
+    if not items:
+        id = 1
+        add['data'] = data
+        add['id'] = id
+    add['data'] = data
+    add['id'] = id
+    items.append(add)
+    
+    return items
+
+@app.route('/items')
+def get_item():
+    if not items:
+        return {}
+    return items
+
+@app.route('/items/<int:id>')
+def get_item_id(id):
+    found=0
+    for item in items:
+        if item['id'] == id:
+            found =1
+            return item
+    if found==0:
+        return jsonify({"error":'item not found'}),404
+
+@app.route("/items/<int:id>", methods=['DELETE'])
+def del_item(id):
+    found=0
+    for item in items:
+        if item['id'] == id:
+            found =1
+            items.remove(item)
+            return jsonify({"message":"item deleted"}),200
+    if found==0:
+        return jsonify({"error":'item not found'}),404
+
+if __name__=='__main__':
+    app.run(debug=True)
