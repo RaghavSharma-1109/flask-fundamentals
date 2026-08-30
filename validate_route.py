f7a633accfed9e9be07613c6200baf7f4cbce133
Raghav Sharma <raghavusharmaa@gmail.com>
2026-08-30 19:10:23 +0530

    Update: Minor logic bugs improved

--- validate_route.py
+++ validate_route.py
@@ -1,43 +1,39 @@
 import json
-from flask import Flask, jsonify,request
+from flask import Flask , jsonify, request 
+
 app = Flask(__name__)
-items =[]
-@app.route('/items', methods=['POST'])
-def add_item():
+items = []
+@app.route('/items' , methods=['POST'])
+def post_item():
     data = request.json
-    if not data or 'name' not in data  :
-        return jsonify({'error':"name is required"}), 400
-    new_id = len(items) + 1
-    add = {'data': data, 'id': new_id}
-    items.append(add)
-    return jsonify(add), 201
+    if not data or 'name' not in data:
+        return jsonify({'error': 'name is required'}), 400
+    id = len(items) +1
+    add_item = {'data': data, 'id': id}
+    items.append(add_item)
+
+    return jsonify(add_item), 201
 
 @app.route('/items')
 def get_item():
     if not items:
-        return {}
-    return items
+        return jsonify({"'error": 'No items'})
+    return jsonify(items)
 
 @app.route('/items/<int:id>')
-def get_item_id(id):
-    found=0
+def get_item_by_id(id):
     for item in items:
         if item['id'] == id:
-            found =1
-            return item
-    if found==0:
-        return jsonify({"error":'item not found'}),404
+            return jsonify(item), 200
+    return jsonify({"error":'item not found'}), 404
 
-@app.route("/items/<int:id>", methods=['DELETE'])
-def del_item(id):
-    found=0
+@app.route('/items/<int:id>', methods=['DELETE'])
+def delete_item_by_id(id):
     for item in items:
         if item['id'] == id:
-            found =1
             items.remove(item)
-            return jsonify({"message":"item deleted"}),200
-    if found==0:
-        return jsonify({"error":'item not found'}),404
+            return jsonify(item), 200
+    return jsonify({"error":'item not found'}), 404
 
 if __name__=='__main__':
     app.run(debug=True)
