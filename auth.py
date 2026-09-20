c01930cbaf4a340ce69488b2f094ff50da4a6e5f
Raghav Sharma <raghavusharmaa@gmail.com>
2026-09-20 09:40:34 +0530

    Update: JWT based token login system for Flask CRUD app

--- auth.py
+++ auth.py
@@ -2,6 +2,8 @@ from flask import Blueprint,request,jsonify
 from extensions import db
 from model import User
 from werkzeug.security import generate_password_hash, check_password_hash
+import jwt
+import datetime
 
 user_bp = Blueprint('users',__name__)
 
@@ -29,4 +31,40 @@ def register_user():
     db.session.commit()
 
     return jsonify({'message': 'User Registered '}), 201
-      
+
+@user_bp.route('/login',methods=['POST'])
+def log_user():
+    data = request.json
+    if not data:
+        return jsonify({'error':'Empty credentials'}), 400
+    if 'username' not in data or 'password' not in data:
+        return jsonify({'error':'Missing important fields'}), 400
+    name = data.get('username')
+    password = data.get('password')
+
+    if not name or not password:
+        return jsonify({'error': 'Empty field credentials'}), 400
+    if not isinstance(name,str) or not isinstance(password,str):
+        return jsonify({'error':'Invalid Field data'}), 400
+    entry = User.query.filter_by(username=name).first()
+    if not entry :
+        return jsonify({'error':'Invalid username or password'}), 401
+    if not check_password_hash(entry.hash_password, password):
+        return jsonify({'error':'Invalid username or password'}), 401
+    
+    token = jwt.encode(
+        {
+            "user_id":entry.id,
+            "exp":datetime.datetime.utcnow() + datetime.timedelta(hours=1)
+        },
+        "Navier-stokes-011235",
+        algorithm="HS256"
+    )
+
+    return jsonify(
+        {
+            'token':token,
+            'message':'User logged in successfully',
+        }
+    ), 200
+    
